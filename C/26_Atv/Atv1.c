c36cee60feda134d8fdb734b354a97260b5fccda
Erysilva98 <erimilsonsilva98@gmail.com>
2022-09-05 22:09:25 -0300

    Codigos

---
+++ C/26_Atv/Atv1.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main (int num1, int num2, int num3){
+   
+   printf("Informe o 1° Número: ");
+   scanf("%d", &num1);
+
+
+   printf("Numero digitado: %d", num1);
+
+   return 0;
+
+}
