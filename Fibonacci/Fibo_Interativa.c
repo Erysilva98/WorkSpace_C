25c52bd4b4642ff2c99604c48cee8d923d801bc5
Erysilva98 <erimilsonsilva98@gmail.com>
2022-09-08 19:26:08 -0300

    1°

---
+++ Fibonacci/Fibo_Interativa.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int fiboInterativa(){
+    
+    int n, i, t1, t2, proxTermo = 0;
+
+    for (i =0; i <= n; i ++){
+        
+        if (proxTermo == 0){
+            return(1);
+        }
+        else{
+            
+        }
+    }
+
+    return 0;
+}
+
+int main(){
+    int n ; 
+
+    printf("\n Função Interativa: Sequência de Fibonacci ");
+   
+    printf("Informe a Quantidade de Termo: ");
+    scanf("%i", &n);
+    fiboInterativa(n);
+    
+    
+    
+    return 0;
+
+}
