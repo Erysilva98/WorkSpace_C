53fc3e9ae74f6c3d14b3cd9ce3f87ec584627178
Erysilva98 <erimilsonsilva98@gmail.com>
2022-09-17 22:50:23 -0300

    M. Ordenação

--- Ordenação/Ordenar.c
+++ Ordenação/Ordenar.c
@@ -1,47 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>       // para clock()
 
-void bublSort(int vetor[], int tam);
-
-int main(void)
+int bublSort(int vetor[], int tam)
 {
-    int vetor[] = {8,46,20,1,87};
+    clock_t tempo;
+    int i, j, temp, troca;
 
-    int i, tam = 5;
+    printf("Iniciando Função Bubble Sort. \n");
 
-    bublSort(vetor, tam);
+    tempo = clock(); // Tempo Inicial
+
+    for (i = 0; i < tam - 1; i++) 
+    {
+        for (j = 0; j < tam - i - 1; j++) 
+        {
+            if (vetor[j] > vetor[j + 1]) 
+            {
+                int temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+            }
+        }
+    }
+
+    tempo = clock() - tempo; // Tempo Final
+
+    printf("Vetor Ordenado com Bubble sort: \n");
+    for (i = 0; i < tam; i++) 
+    {
+        printf("%d  ", vetor[i]);
+    }
+    printf("\n  Tempo de Execução: %lf ",((double)tempo)/((CLOCKS_PER_SEC/1000)));
     printf("\n");
+    return 0;
 
+}
+
+int insertionSort(int vetor[], int tam)
+{
+    clock_t tempo;
+    int i, j, troca;
+
+    tempo = clock(); // Tempo Inicial
+
+    printf("\nIniciando Função Bubble Sort. \n");
+
+    for(i =1; i < tam; i++){
+        troca = vetor[i];
+        j = i -1;
+
+        while ( j>=0 && vetor[j] > troca )
+        {
+            vetor[j+1] = vetor[j];
+            // Elemento da lista Ordenado
+            j = j -1;
+        }
+        vetor[j+1] = troca;
+    }
+
+    tempo = clock() - tempo; // Tempo Final
+
+    printf("Vetor Ordenado com Insert Sort: \n");
+    for (i = 0; i < tam; i++) 
+    {
+        printf("%d  ", vetor[i]);
+    }
+    printf("\n  Tempo de Execução: %lf ",((double)tempo)/((CLOCKS_PER_SEC/1000)));
+    printf("\n");
     return 0;
 }
 
-void bublSort(int vetor[], int tam)
+int selectionSort(int *vetor, int tam)
 {
-    int temp, troca;
+    clock_t tempo;
+    int i, j, ultimo, temp;
 
-    printf("Iniciando Função Bubble Sort \n");
+    printf("\nIniciando Função Selection Sort. \n");
 
-    troca = 1;
+    tempo = clock(); // Tempo Inicial
 
-    while(troca !=0)
+    for(i=0; i<(tam-1); i++)
     {
-        troca = 0;
-        for(int i=0; i<tam; i++)
+        ultimo = i;
+
+        for(j= (i+1); j < tam; j++)
         {
-            if(vetor[i] > vetor[i+1])
-            {
-                troca =1;
-                temp = vetor[i];
-                vetor[i] = vetor[i+1];
-                vetor[i+1] = temp;
-            }
+            if(vetor[j] < vetor[ultimo])
+            ultimo = j;
         }
-        tam = tam -1;
+        if (vetor[i] != vetor[ultimo] )
+        {
+            temp = vetor[i];
+            vetor[i] = vetor[ultimo];
+            vetor[ultimo] = temp;
+        }
     }
 
-    printf("Lista Ordenada en Ordem Crescente : ");
-    for(int i =0; i<=tam; i++)
+    tempo = clock() - tempo; // Tempo Final
+
+    printf("Vetor Ordenado com Selection Sort: \n");
+    for (i = 0; i < tam; i++) 
     {
-        printf("%d", vetor[i]);
+        printf("%d  ", vetor[i]);
     }
+    printf("\n  Tempo de Execução: %lf ",((double)tempo)/((CLOCKS_PER_SEC/1000)));
+    printf("\n");
+    return 0;
+}
+
+int main()
+{
+    int vetor[] = {8,6,2,1,7,9,5,3,10,4,23,99,45,67,21,76,57,71,49,20};
+
+    int i, tam = 20;
+
+    printf("Vetor á ser Ordenado: \n");
+    for (i = 0; i < tam; i++) 
+    {
+        printf("%d  ", vetor[i]);
+    }
+    printf("\n \n");
+
+    bublSort(vetor, tam);
+    insertionSort(vetor, tam);
+    selectionSort(vetor, tam);
+
+    return 0;
 }
---
+++ Ordenação/Readme.md
@@ -0,0 +1,7 @@
+# Implementação de Algoritmos de Ordenação
+
+Implemente de forma iterativa os algoritmos de ordenação.
+
+1. Insertion Sort
+2. Selection Sort
+3. Bubble Sort
