6dc93d3bb20d4da571326ea5607538f16b52ab3a
Erysilva98 <erimilsonsilva98@gmail.com>
2022-09-19 20:59:24 -0300

    Merge Sort OK

--- Ordenação/mergeSort.c
+++ Ordenação/mergeSort.c
@@ -1,87 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mergeSort(int vetor[], int tam);
-void mergeSortRecursivo( int vetor[], int inicio, int final);
-void ordenarVetor(int vetor[], int inicio, int meio, int final);
+void Intercala(int vetor[], int inicio, int meio, int final);
+void mergeSort(int vetor[], int inicio, int final);
+void printArray(int vetor[], int tam);
 
 int main()
 {
     int vetor[] = {9, 4, 8, 1, 7, 0, 3, 2, 5, 6};
 
-    int i, tam = 10;
+    int i, pos = 0, tam = 10;
+
+    printf("\n Vetor a ser Ordenado\n");
+    printArray(vetor, tam);
+
+    mergeSort(vetor, pos, tam);
 
     printf("\n Vetor Ordenado com Merge Sort \n");
+    printArray(vetor, tam);
 
-    mergeSort(vetor, tam);
-
-    for( i=0; i < tam; i++)
-    {
-        printf("%d ", vetor[i]);
-
-    }
     printf("\n");
 
-    return 0;
-
+   return 0;
 }
 
-void mergeSort(int vetor[], int tam)
+void Intercala(int vetor[], int inicio, int meio, int final)
 {
-    mergeSortRecursivo(vetor, 0, tam - 1);
-}
+    int i, j, k, *w;
 
-void mergeSortRecursivo( int vetor[], int inicio, int final)
-{
-    if ( inicio < final)
+    w = malloc ( (final-inicio) * sizeof (int) );
+
+    i = inicio; j= meio; k = 0;
+
+    while (i < meio && j < final)
     {
-        int meio = inicio + (final - inicio ) / 2;
-
-        mergeSortRecursivo(vetor, inicio, meio);
-        mergeSortRecursivo(vetor, meio + 1, final);
-
-        ordenarVetor(vetor, inicio, meio, final);
-    }
-}
-
-void ordenarVetor(int vetor[], int inicio, int meio, int final)
-{
-    // Tamanho dos Vetor
-
-    int tamInicio = meio - inicio + 1;
-    int tamFinal = final - meio;
-
-    // Variáveis Temporárias
-
-    int temp_inicio[tamInicio];
-    int temp_final[tamFinal];
-
-    // Variáveis do FOR
-
-    int i, j, k;
-
-    for (i = 0; i < tamInicio; i++)
-    {
-        temp_inicio[i] = vetor[inicio + i];
-    }
-
-    for (i = 0; i < tamFinal; i++)
-    {
-        temp_final[i] = vetor[meio + 1 + i];
-    }
-
-    for (i =0, j= 0, k= inicio; k <= final; k++)
-    {
-        if (( i < tamInicio) && (j >= tamFinal) || temp_inicio[i] <= temp_final[j])
+        if ( vetor[i] <= vetor[j])
         {
-            vetor[k] = temp_inicio[i];
-            i++;
+            w[k++] = vetor[i++];
         }
         else
         {
-            vetor[k] = temp_final[j];
-            j++;
+            w[k++] = vetor[j++];
         }
+    }
+    while (i < meio)
+    {
+        w[k++] = vetor[i++];
+    }
+    while (j < final)
+    {
+        w[k++] = vetor[j++];
+    }
+
+    for ( i=inicio; i < final; i++)
+    {
+        vetor[i] = w[i-inicio];
+    }
+    free (w);
+}
+
+void mergeSort(int vetor[], int inicio, int final)
+{
+    if (inicio < final -1 )
+    {
+        int meio = (inicio + final) / 2;
+
+        mergeSort(vetor, inicio, meio);
+        mergeSort(vetor, meio, final);
+        Intercala(vetor, inicio, meio, final);
     }
 }
 
+void printArray(int vetor[], int tam)
+{
+    int i;
+
+    for( i=0; i < tam; i++)
+    {
+        printf(" %d", vetor[i]);
+
+    }
+    printf("\n");
+}
